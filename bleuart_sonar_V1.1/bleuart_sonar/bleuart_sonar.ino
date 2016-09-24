a573d4edede94705cdd5624051106dad7772bb1d
benbeezy <benengel94@gmail.com>
2016-09-24 12:39:16 -0600

    took out button push that was showing up in the seriel logs. It was my first test to do io back to the phone but it not needed anymore

--- bleuart_sonar_V1.1/bleuart_sonar/bleuart_sonar.ino
+++ bleuart_sonar_V1.1/bleuart_sonar/bleuart_sonar.ino
@@ -72,17 +72,6 @@
     #define FACTORYRESET_ENABLE         1
     #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
     #define MODE_LED_BEHAVIOUR          "MODE"
-
-    
-// constants won't change. They're used here to
-// set pin numbers:
-const int buttonPin = 5;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-
-
 /*=========================================================================*/
 
 // Create the bluefruit object, either software serial...uncomment these lines
@@ -125,11 +114,6 @@ void setup(void)
 {
   //while (!Serial);  // required for Flora & Micro
   delay(200);
-
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
 
   Serial.begin(115200);
   Serial.println(F("Adafruit Bluefruit Command Mode Example"));
@@ -219,12 +203,6 @@ void loop(void)
         Serial.println("Hz");
         }
   //sonar
-
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
   
   // Check for user input
   char inputs[BUFSIZE+1];
